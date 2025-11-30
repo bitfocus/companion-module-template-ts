1435b9ff5196ee13ca1c12a4862e06b0a72f3890
phillipivan <phillip.pietruschka@parliament.vic.gov.au>
2025-11-30 11:40:08 +1100

    add module secrets framework to template

--- src/upgrades.ts
+++ src/upgrades.ts
@@ -1,7 +1,7 @@
 import type { CompanionStaticUpgradeScript } from '@companion-module/base'
-import type { ModuleConfig } from './config.js'
+import type { ModuleConfig, ModuleSecrets } from './config.js'
 
-export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig>[] = [
+export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig, ModuleSecrets>[] = [
 	/*
 	 * Place your upgrade scripts here
 	 * Remember that once it has been added it cannot be removed!
